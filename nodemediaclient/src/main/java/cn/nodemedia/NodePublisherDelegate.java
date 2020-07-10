876dda87b3af867140c8607fe43baf937e582c86
naveensaini4u <nsaini386@gmail.com>
2020-07-10 13:09:18 +0530

    fix issue

--- nodemediaclient/src/main/java/cn/nodemedia/NodePublisherDelegate.java
+++
@@ -1,9 +0,0 @@
-package cn.nodemedia;
-
-/**
- * Created by ${$USER_NAME} on 17/4/19.
- */
-
-public interface NodePublisherDelegate {
-    void onEventCallback(NodePublisher streamer, int event, String msg);
-}
